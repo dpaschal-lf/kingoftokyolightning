a60add6dca2b1967e894efe4ab941e102cb73da0
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-09-02 08:49:54 -0700

    added point changing

--- monster.js
+++ monster.js
@@ -3,6 +3,10 @@ class Monster{
     constructor( name, img, initialHitpoints, initialVictorypoints){
         this.name = name;
         this.image = img;
+        this.maximums = {
+            life: 10,
+            victory: 20
+        }
         this.points = {
             life: initialHitpoints,
             victory: initialVictorypoints
@@ -12,6 +16,27 @@ class Monster{
             lifePoints: null,
             victoryPoints: null
         }
+    }
+    changeHitpoints( newValue ){
+        
+        this.changePoints( 'life', newValue );
+    }
+    changeVictorypoints( newValue ){
+        this.changePoints( 'victory', newValue );
+    }
+    changePoints( type, newValue ){
+        var nextValue = this.points[type] + newValue;
+        if( nextValue > this.maximums[type] ){
+            nextValue = this.maximums[type];
+        } else if (nextValue < 0){
+            nextValue = 0;
+        }
+        this.points[type] = nextValue;
+        this.update();
+    }
+    update(){
+        this.domElements.lifePoints.text(this.points.life);
+        this.domElements.victoryPoints.text(this.points.victory);
     }
     render(){
         this.domElements.container = $("<div>",{
